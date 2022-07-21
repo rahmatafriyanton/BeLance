047faab2bd7bec4377bd863b81a87466dd3d2102
Rahmat Afriyanton <rahmatafriyanton@gmail.com>
2022-07-21 11:27:49 +0700

    Initial Xcode Commit

---
+++ BeLance/BeLanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeLanceApp.swift
+//  BeLance
+//
+//  Created by Rahmat Afriyanton on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BeLanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
